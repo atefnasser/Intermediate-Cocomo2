e90507bdfabce111b25041fab3668a315fa9f2ca
Atef Nasser <atefnass@umich.edu>
2023-12-08 15:51:41 -0500

    Added Navbar
    Removed sidebar and added a sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Toolbar from './components/header/header';
-import Sidebar from './components/sidebar/sidebar';
 import MainPage from './pages/main/main-page';
 import About from './components/about/About';
 import Footer from './components/footer/Footer';
+import NavBar from './components/navbar/NavBar'; // Ensure you have created this component
 import "./App.css";
 
 function App() {
-    const [isSidebarOpen, setSidebarOpen] = useState(false);
-  
-    const toggleSidebar = () => {
-      setSidebarOpen(!isSidebarOpen);
-    };
-
   return (
     <Router>
-      <div className='flex'>
-        {/* Toggle button for the sidebar */}
-        <button onClick={toggleSidebar}>Open Sidebar</button>
-
-        {/* Pass the isOpen and toggleSidebar props to the Sidebar */}
-        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-
-        {/* Main content area */}
-        <div className='flex flex-col min-h-screen w-full'>
-          <Toolbar />
-          <Routes> {/* Updated component name */}
-            <Route exact path="/" element={<MainPage />} />
-            <Route path="/about" element={<About />} />
-            {/* Update other routes as needed */}
-          </Routes>
-          <Footer />
-        </div>
+      <div className='flex flex-col min-h-screen w-full'>
+        <Toolbar />
+        <NavBar /> {/* Include the NavBar component */}
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<About />} />
+          {/* Add more routes as needed */}
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css'; // Make sure you have a corresponding CSS file for styles
+
+const NavBar = () => {
+  return (
+    <nav className="nav-bar">
+      <NavLink to="/" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+        Home
+      </NavLink>
+      <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+        About
+      </NavLink>
+      {/* Add more navigation links as needed */}
+    </nav>
+  );
+};
+
+export default NavBar;
